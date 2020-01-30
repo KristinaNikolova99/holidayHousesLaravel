e1d9f9ae6f0d75b666c84e5515efa0359978d883
KristinaNikolova99 <60451437+KristinaNikolova99@users.noreply.github.com>
2020-01-30 16:57:10 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@admin')    
     ->middleware('is_admin')    
     ->name('admin');
+
+Route::resource('/holidayhouses', 'HolidayHousesController');
+Route::resource('/locations', 'LocationsController');
+Route::resource('/types', 'TypesController');
