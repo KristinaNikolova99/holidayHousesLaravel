ee9fe904785a0906cc46693627520223078869a8
KristinaNikolova99 <60451437+KristinaNikolova99@users.noreply.github.com>
2020-01-30 16:37:34 +0200

    LocationsController

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Location;
 use Illuminate\Http\Request;
 
 class LocationsController extends Controller
@@ -13,7 +14,9 @@ class LocationsController extends Controller
      */
     public function index()
     {
-        //
+                $locations = Location::all();
+
+        return view('locations.index', compact('locations'));
     }
 
     /**
@@ -23,7 +26,7 @@ class LocationsController extends Controller
      */
     public function create()
     {
-        //
+        return view('Locations.create');
     }
 
     /**
@@ -34,16 +37,20 @@ class LocationsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+                \App\Location::create([
+          'location' => $request->get('location'),
+        ]);
+
+        return redirect('/locations')->with('success', 'Location has been added');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\location  $location
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Location $location)
     {
         //
     }
@@ -51,34 +58,48 @@ class LocationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Location  $location
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+         $location = Location::find($id);
+
+        return view('locations.edit', compact('location'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Location  $location
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+         $request->validate([
+        'location'=>'required',
+      ]);
+
+      $location = Location::find($id);
+      $location->location = $request->get('location');
+
+      $location->save();
+
+      return redirect('/locations')->with('success', 'Location has been updated');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Location  $location
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $location = Location::find($id);
+     $location->delete();
+
+     return redirect('/locations')->with('success', 'Location has been deleted Successfully');
     }
 }
