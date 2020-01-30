2d01f749c63ea4303d0eb412711615fbac4d5629
KristinaNikolova99 <60451437+KristinaNikolova99@users.noreply.github.com>
2020-01-30 17:49:55 +0200

    HolidayHouses create view

---
+++ resources/views/HolidayHouses/create.blade.php
@@ -0,0 +1,56 @@
+  
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Create Holiday House
+                        <a href="{{ URL::to('holidayhouses') }}" class="pull-right">List all</a>
+                    </div>
+
+                    <div class="panel-body">
+                        <!-- will be used to show any messages -->
+                        @if (Session::has('message'))
+                            <div class="alert alert-info">{{ Session::get('message') }}</div>
+                        @endif
+
+                        
+                        <form method="post" action="{{url('holidayhouses')}}">
+                            <div class="form-group row">
+                                {{csrf_field()}}
+                                <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="name" name="name">
+                                </div>
+                                <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Location</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="location_id" name="location_id">
+                                </div>
+                                <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Number of rooms</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="numberOfRooms" name="numberOfRooms">
+                                </div>
+                                <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Number of beds</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="numberOfBeds" name="numberOfBeds">
+                                </div>
+                                <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Type</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="type_id" name="type_id">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-2"></div>
+                                <input type="submit" class="btn btn-primary">
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
